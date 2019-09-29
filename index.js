7bf34cfc62bbf9bd04afbaa691c875956433fa7f
Master <andikanaffiah@gmail.com>
2019-09-29 07:09:16 +0100

    Completed

--- index.js
+++ index.js
@@ -1,53 +1,81 @@
 const addButton = document.querySelector('#addButton');
 const selectDisplay = document.querySelector('#display');
 const calculateButton = document.querySelector('#calculateButton');
+const itemDisplay = document.querySelector('#item-display');
+const toolTip = document.querySelector('#item-display .tooltiptext');
+const displayModal = document.querySelector('.display-modal');
+const display2 = document.querySelector('.display2');
 
 let electronics = [];
 
 addButton.addEventListener('click', () => {
 	const name = document.querySelector('.name-input input');
-	const quantity = document.querySelector('.quantity-watt input:first-of-type');
-	const watt = document.querySelector('.quantity-watt input:last-of-type');
+	const quantity = document.querySelector('.quantity-watt input:nth-child(1)');
+	const watt = document.querySelector('.quantity-watt input:nth-child(2)');
+	const days = document.querySelector('.quantity-watt input:nth-child(3)');
+	const itemDisplaySpan = document.querySelector('#item-display > span:nth-child(2)');
 
-        if(name.value != "" && quantity.value != "" && watt.value != ""){
-	    electronics.push([name.value, quantity.value, watt.value]);
-            selectDisplay.innerHTML = "";
-	    name.innerHTML = "";
-	    quantity.innerHTML = "";
-	    watt.innerHTML = "";
 
-	    electronics.forEach(item => {
-		let span = document.createElement('span');
-		span.innerHTML = item[0] + " " + "(" + item[1] + ")";
-		span.classList.add('bg-tone-blue');
-		selectDisplay.appendChild(span);
-	    });
-        }else{ 
-            alert("fields must not be empty");
-        }
+	if(name.value != "" && quantity.value != "" && watt.value != ""){
+		electronics.push([name.value, quantity.value, watt.value, days.value]);
 
-	
-   
-	
+		selectDisplay.value = "";
+		name.value = "";
+		quantity.value = "";
+		watt.value = "";
+		days.value = "";
+
+		electronics.forEach(item => {
+			let div = document.createElement('div');
+			div.innerHTML = item[0] + " " + "(" + item[1] + ")";
+			div.classList.add('bg-tone-blue');
+			selectDisplay.appendChild(div);
+		});
+
+		itemDisplaySpan.innerText = electronics.length;
+	}else{ 
+	    alert("fields must not be empty");
+	}
+
+	selectDisplay.classList.replace('show', 'hidden');
+
+	toolTip.classList.replace('hidden', 'show');
+	setTimeout(() => {
+		toolTip.classList.replace('show', 'hidden');
+	}, 1000);
+
 });
 
 calculateButton.addEventListener('click',() => {
 
+	selectDisplay.classList.replace('show', 'hidden');
+
+
 	const productArray = [];
 	let sum = 0;
+	let sumDays = 0;
 	electronics.forEach(item => {
 		let product = item[1] * item[2];
 		productArray.push(product);
+
+		if(item[3] > sumDays){
+			sumDays += item[3];
+		}
 	});
 
 	for (let i = 0; i < productArray.length; i++) {
 		sum += productArray[i];
 	}
 
-	alert((sum / 1000) * 24 + "kWh Per day");
-	alert((sum / 1000) * 24 * 7 + "kWh Per Week");
-	alert((sum / 1000) * 24 * 30 + "kWh Per Month");
-	
+	let val = document.querySelector('.val');
+	let val1 = document.querySelector('.val1');
+	let val2 = document.querySelector('.val2');
+
+	val.innerText = ((sum / 1000) * 24).toFixed(2) + "kWh Per day";
+	val1.innerText = ((sum / 1000) * 24 * 7).toFixed(2) + "kWh Per Week";
+	val2.innerText = ((sum / 1000) * 24 * 30).toFixed(2) + "kWh Per Month";
+
+	display2.classList.replace('hidden', 'shown');
 	// var params = typeof electronics == 'string' ? electronics : Object.keys(electronics).map(
 	// 	function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(electronics[k]) }
 	// 	).join('&');
@@ -60,5 +88,18 @@ calculateButton.addEventListener('click',() => {
 	// 	}
 	// };
 	// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	// xhr.send(params);	
+	// xhr.send(params);
+
+});
+
+itemDisplay.addEventListener('click', () => {
+	if (selectDisplay.className == 'hidden'){
+		selectDisplay.classList.replace('hidden', 'show');
+	}else{
+		selectDisplay.classList.replace('show', 'hidden');
+	}
+});
+
+display2.addEventListener('click', () => {
+	display2.classList.replace('shown', 'hidden');
 });
